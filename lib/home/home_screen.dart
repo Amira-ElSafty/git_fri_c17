ed2fac8016bacbc2f1e699e589a5e9fab030df83
DESKTOP-NR6TVAI\LAPTOP <amiraelsaftytraining@gmail.com>
2025-12-12 14:44:42 +0200

    + Added appBar in scaffold.

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -6,6 +6,9 @@ class HomeScreen extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.orange,
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+      ),
     );
   }
 }
